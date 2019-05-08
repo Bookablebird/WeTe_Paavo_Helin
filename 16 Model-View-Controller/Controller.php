80cd8f7c5638e9989b62c8cbc2fe222388565733
Paavo Helin <paavodotcom@gmail.com>
2019-05-08 19:10:57 +0300

    Update Controller.php

--- 16 Model-View-Controller/Controller.php
+++ 16 Model-View-Controller/Controller.php
@@ -12,9 +12,14 @@ class Controller {
     }
 
     public function send() {
-        $nick = $_POST["nick"];
-        setcookie("nick", $nick);
-        $this->model->add_message($nick, $_POST["message"]);
+		$nick = $_POST["nick"];
+        if ($nick != ""){
+            setcookie("nick", $nick);
+            $user = $nick;
+        } else {
+            $user = $_COOKIE['nick'];
+        }
+        $this->model->add_message($user, $_POST["message"]);
         header("Location: Chat.php?action=list_it");
     }
 }
